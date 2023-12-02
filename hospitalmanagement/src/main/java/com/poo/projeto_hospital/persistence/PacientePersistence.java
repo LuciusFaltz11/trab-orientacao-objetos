0c282b18686eddd609eb177bcf751d779ec0a9e6
Lucius Faltz <lucius@agenciaoasis.com.br>
2023-12-02 16:55:43 -0300

    add: implementação de lógica de login

--- hospitalmanagement/src/main/java/com/poo/projeto_hospital/persistence/PacientePersistence.java
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/persistence/PacientePersistence.java
@@ -3,17 +3,18 @@ package com.poo.projeto_hospital.persistence;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.poo.projeto_hospital.model.Paciente;
+import com.poo.projeto_hospital.model.Usuario;
 
 import java.io.File;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PacientePersistence implements Persistence<Paciente> {
+public class PacientePersistence implements Persistence<Usuario> {
     private static final String PATH = DIRECTORY+ File.separator +"pacientes.json";
 
     @Override
-    public void save(List<Paciente> itens) {
+    public void save(List<Usuario> itens) {
         Gson gson = new Gson();
         String json = gson.toJson(itens);
 
@@ -23,19 +24,18 @@ public class PacientePersistence implements Persistence<Paciente> {
 
         Arquivo.salva(PATH, json);
 
-
     }
 
     @Override
-    public List<Paciente> findAll() {
+    public List<Usuario> findAll() {
         Gson gson = new Gson();
 
         String json = Arquivo.le(PATH);
 
-        List<Paciente> pacientes = new ArrayList<>();
+        List<Usuario> pacientes = new ArrayList<>();
         if(!json.trim().equals("")) {
 
-            Type tipoLista = new TypeToken<List<Paciente>>() {
+            Type tipoLista = new TypeToken<List<Usuario>>() {
             }.getType();
             pacientes = gson.fromJson(json, tipoLista);
 
