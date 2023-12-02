dbac7702fbaa06acf5b9b50016bb40ce5f86318f
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-12-02 12:05:08 -0300

    criação de classes em controller para actions

--- hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
@@ -1,11 +1,16 @@
 package com.poo.projeto_hospital;
 
+import com.poo.projeto_hospital.view.paginaInicial_login.Login;
+import com.poo.projeto_hospital.view.paginaInicial_login.RegistroPaciente;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
+        Login login = new Login();
+        login.createAndShowGUI();
 
     }
 }
---
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/controller/EntrarLogin.java
@@ -0,0 +1,21 @@
+package com.poo.projeto_hospital.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.poo.projeto_hospital.view.paginaInicial_login.Login;
+
+public class EntrarLogin implements ActionListener {
+    private Login login;
+
+    public EntrarLogin(Login login) {
+        this.login = login;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+}
