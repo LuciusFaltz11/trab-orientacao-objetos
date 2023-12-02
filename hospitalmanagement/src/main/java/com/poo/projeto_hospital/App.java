4c5468923839183eccfc2496711bb849e92e1b95
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-12-02 14:49:11 -0300

    arrumar arquivo pom

--- hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
@@ -1,16 +1,10 @@
 package com.poo.projeto_hospital;
 
 import com.poo.projeto_hospital.view.paginaInicial_login.Login;
-import com.poo.projeto_hospital.view.paginaInicial_login.RegistroPaciente;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         Login login = new Login();
         login.createAndShowGUI();
-
     }
 }
