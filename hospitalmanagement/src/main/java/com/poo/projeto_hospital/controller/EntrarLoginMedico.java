a9825fa32133f4f67c1034f0105fc061911576e3
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-12-03 16:28:40 -0300

    adiciona classes para login de médicos

---
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/controller/EntrarLoginMedico.java
@@ -0,0 +1,25 @@
+package com.poo.projeto_hospital.controller;
+
+import java.awt.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import com.poo.projeto_hospital.model.Usuario;
+import com.poo.projeto_hospital.persistence.MedicoPersistence;
+import com.poo.projeto_hospital.persistence.Persistence;
+import com.poo.projeto_hospital.view.paginaInicial_login.LoginMedico;
+
+public class EntrarLoginMedico implements ActionListener {
+    private LoginMedico loginMedico;
+
+    public EntrarLoginMedico(LoginMedico loginMedico) {
+        this.loginMedico = loginMedico;
+    }
+
+    @Override
+    
+
+}
---
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/persistence/MedicoPersistence.java
@@ -0,0 +1,5 @@
+package com.poo.projeto_hospital.persistence;
+
+public class MedicoPersistence {
+
+}
