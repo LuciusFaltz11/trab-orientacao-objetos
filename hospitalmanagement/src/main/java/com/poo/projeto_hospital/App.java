dffb34d2e9ae1b92a6b3a43b1380d021ac8ea081
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-12-07 20:14:27 -0300

    pagina inicial do paciente

--- hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/App.java
@@ -1,6 +1,9 @@
 package com.poo.projeto_hospital;
 
 import com.poo.projeto_hospital.model.Medico;
+import com.poo.projeto_hospital.model.Paciente;
+import com.poo.projeto_hospital.model.Usuario;
+import com.poo.projeto_hospital.model.UsuarioMedico;
 import com.poo.projeto_hospital.view.TelaAgenda;
 import com.poo.projeto_hospital.view.paginaInicial_login.Login;
 import com.poo.projeto_hospital.view.perfilPaciente.AreaPaciente;
@@ -14,7 +17,8 @@ public class App {
         // "Cardiologista", "08:00", "18:00");
         // medico.getAgenda();
 
-        AreaPaciente areaPaciente = new AreaPaciente();
+        Paciente paciente = new Paciente("Luiza", "18785173762", "20/02/2020", "São Paulo", "SP", "Feminino");
+        AreaPaciente areaPaciente = new AreaPaciente(paciente);
         areaPaciente.createAndShowGUI();
 
     }
