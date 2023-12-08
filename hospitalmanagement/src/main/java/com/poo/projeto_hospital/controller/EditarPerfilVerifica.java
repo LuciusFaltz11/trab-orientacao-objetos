17b674b3d07da103944c12f2f6d0a1116773a90e
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-12-08 15:59:08 -0300

    criação de classe para validação de alterarDados

--- hospitalmanagement/src/main/java/com/poo/projeto_hospital/controller/EditarPerfilVerifica.java
+++ hospitalmanagement/src/main/java/com/poo/projeto_hospital/controller/EditarPerfilVerifica.java
@@ -3,6 +3,12 @@ package com.poo.projeto_hospital.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import com.poo.projeto_hospital.exception.EmailException;
+import com.poo.projeto_hospital.exception.SenhaException;
+import com.poo.projeto_hospital.model.Email;
+import com.poo.projeto_hospital.model.Senha;
 import com.poo.projeto_hospital.model.Usuario;
 import com.poo.projeto_hospital.model.UsuarioMedico;
 import com.poo.projeto_hospital.view.perfilPaciente.EditarPerfil;
@@ -16,8 +22,30 @@ public class EditarPerfilVerifica implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        String cidade = alteraPerfil.getCidadeTextField().getText();
+        String estado = alteraPerfil.getEstadoTextField().getText();
+        String sexo = alteraPerfil.getGenderTextField().getText();
+        String email = alteraPerfil.getEmailTextField().getText();
+        char[] senha = alteraPerfil.getSenhaTextField().getPassword();
+
+        try {
+            email = Email.isValidEmail(email);
+            Senha.isValidSenha(senha);
+        } catch (EmailException ex) {
+            ex.getMessage();
+            return;
+        } catch (SenhaException ex) {
+            ex.getMessage();
+            return;
+        }
+
+        if (!sexo.equals("f") && !sexo.equals("F") && !sexo.equals("m") && !sexo.equals("M")) {
+            JOptionPane.showMessageDialog(null, "Sexo inválido!", "Sexo Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        // trocar as informações do paciente e salvar no paciente
+
     }
 
 }
